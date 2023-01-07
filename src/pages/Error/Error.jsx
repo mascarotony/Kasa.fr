f1197c76e4d874d4df908af12312402205fb244f
Mascaro Tony <mascaro.tony@outlook.fr>
2023-01-07 17:05:08 +0100

    25% - Finalisation du Header + Footer + Nav
    25% OK ->
    
    - Ajout du Footer pour chaque page
    
    - Feuilles de styles OK pour :
      ->Header
      ->Footer
      ->Nav
    
    Projet prêt pour étapes des 40% : Création du contenu de la page d'acceuil "Home"

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,11 +1,13 @@
 import './Error.css'
 import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 function Error() {
   return (
     <div>
       <Header />
       <h1>Erreur 404</h1>
+      <Footer />
     </div>
   )
 }
