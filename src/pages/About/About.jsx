71c737cb5a4196a255fe7af1fa3320dd4c9fcb96
Mascaro Tony <mascaro.tony@outlook.fr>
2023-01-09 19:37:38 +0100

    50% (NF) - Remplacement Favicon + quelques styles revue
    50% (Not Finish) ->
    
    - Remplacement du Favicon pour intégrer l'icône Kasa
    
    - Suppression de fichier en surplus (ancien icône) + allegement du fichier html avec le retraie des snippets liés à l'ancien icône
    
    - Retouche de quelques feuilles de style pour un rendu plus propre de la page d'acceuil (responsive pc portable et écran large uniquement)

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import aboutBanner from '../../assets/About_Banner.svg'
+import aboutBanner from '../../assets/About_Banner.webp'
 
 import './About.css'
 
