2228d2337e83641b1c32390121d90fe1afd3f7ec
Mascaro Tony <mascaro.tony@outlook.fr>
2023-01-10 09:14:20 +0100

    50% - Finalisation de la page About + Responsive PC Portable et écran large
    50% OK ->
    
    - Création de la page About terminée + application du style
    
    - Mise en place du responsive PC Portable et écran large
    
    - Responsive tablette et mobile à venir à la fin du projet

---
+++ src/components/DropPanel/DropPanel.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+import Vector from '../../assets/Vector.svg'
+
+import './DropPanel.css'
+
+function DropPanel(props) {
+  const [display, setDisplay] = useState('none')
+  const [isActive, setIsActive] = useState('')
+  const [rotate, setRotate] = useState('180deg')
+
+  function inputDropPanel() {
+    setIsActive(isActive === '' ? 'active' : '')
+    setRotate(rotate === '180deg' ? '0deg' : '180deg')
+    setDisplay(display === 'block' ? 'none' : 'block')
+  }
+
+  return (
+    <div className="dropPanel">
+      <div className="dropPanel-Block" onClick={inputDropPanel}>
+        <h1 id="title">{props.title}</h1>
+        <button>
+          <img
+            className="Vector"
+            src={Vector}
+            style={{ rotate: `${rotate}` }}
+            alt="chevron"
+          />
+        </button>
+      </div>
+      <div
+        className={`text ${isActive}`}
+        key={props.id}
+        style={{ display: `${display}` }}
+      >
+        {props.texte}
+      </div>
+    </div>
+  )
+}
+
+export default DropPanel
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,42 @@
+//Utils
 import React from 'react'
+import { useEffect, useState } from 'react'
 
+//Components
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import aboutBanner from '../../assets/About_Banner.webp'
+import Panel from '../../components/DropPanel/DropPanel'
 
+//Assets
+import aboutBanner from '../../assets/About_Banner.webp'
+import AboutItems from '../../assets/datas/about'
+
+//Styles
 import './About.css'
 
+//Function
 function About() {
+  const Items = AboutItems()
+  const [mode, setMode] = useState('desktop')
+
+  useEffect(() => {
+    if (window.innerWidth >= 700) {
+      setMode('desktop')
+    } else {
+      setMode('mobile')
+    }
+    window.addEventListener('resize', onResize)
+
+    function onResize() {
+      if (window.innerWidth >= 700 && mode === 'mobile') {
+        setMode('desktop')
+      }
+      if (window.innerWidth < 700 && mode === 'desktop') {
+        setMode('mobile')
+      }
+    }
+  }, [mode])
+
   return (
     <div className="aboutPage">
       <Header />
@@ -17,6 +47,9 @@ function About() {
           alt="Paysage montagneux ensoleillé"
         />
       </div>
+      {Items.map((item) => {
+        return <Panel title={item.title} texte={item.text} key={item.id} />
+      })}
       <Footer />
     </div>
   )
