20f29f36dd056584f2c6334c091718fb235e77d8
Mascaro Tony <mascaro.tony@outlook.fr>
2023-01-12 16:30:12 +0100

    85%(NF) - Création de la structure Logement
    85% - (Not Finish) ->
    
    -> Création de la structure de la page Logement
    
    -> Style + Responsive à venir dans les prochains commits

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,20 +1,75 @@
 //Utils
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 //Components
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Carousel from '../../components/Carousel/Carousel'
+import DropPanel from '../../components/DropPanel/DropPanel'
+
+//Page
+import Error from '../Error/Error'
+
+//Assets & Datas
+import cardItems from '../../assets/datas/logements'
 
 //Style
 import './Logement.css'
 
 //Function
 function Logement() {
+  const { id } = useParams()
+  const [item, setItem] = useState()
+
+  useEffect(() => {
+    const foundItem = cardItems.find((card) => card.id === id)
+
+    setItem(foundItem)
+  }, [id])
+
+  if (!item) {
+    return <Error />
+  }
+
+  const listEquipments = item.equipments.map((equipment) => (
+    <li key={equipment}>{equipment}</li>
+  ))
+
   return (
-    <div>
+    <div className="logementPage">
       <Header />
-      <Carousel />
+      <Carousel pictures={item.pictures}></Carousel>
+      <div className="content">
+        <div className="leftBloc">
+          <div className="location">
+            <b>{item.title}</b>
+            <p>{item.location}</p>
+          </div>
+          <div className="tags">
+            <ul>
+              {item.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="rigthBloc">
+          <div className="LvlRating"></div>
+
+          <div className="host">
+            <p>{item.host.name}</p>
+            <img src={item.host.picture} alt="host" />
+          </div>
+        </div>
+      </div>
+
+      <div className="descriptionBloc">
+        <DropPanel texte={item.description} title="Déscription" />
+        <DropPanel texte={listEquipments} title="Équipements" />
+      </div>
       <Footer />
     </div>
   )
